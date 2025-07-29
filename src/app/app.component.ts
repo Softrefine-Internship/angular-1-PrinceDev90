03a0bdae33031e09211db956fd3b6c2507b3fae6
PrinceDevSoftrefine <ubuntubhai.developer@gmail.com>
2025-07-29 16:02:36 +0530

    test end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,102 @@
 import { Component } from '@angular/core';
+import { Person } from './app.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-1';
+  name: string = '';
+  email: string = '';
+  phone: string = '';
+  age: string = '';
+  isModel: boolean = false;
+
+  selectedData: Person = {
+    id: 1,
+    name: '',
+    email: '',
+    phone: '',
+    age: 1,
+  };
+
+  data: Person[] = [
+    {
+      id: 1,
+      name: 'Terry Gomez',
+      email: 'terry@example.com',
+      phone: '+1 987 654 3210',
+      age: 34,
+    },
+    {
+      id: 2,
+      name: 'Alyssa Silva',
+      email: 'alyssa@example.com',
+      phone: '+1 456 123 7890',
+      age: 27,
+    },
+    {
+      id: 3,
+      name: 'Donald Richardson',
+      email: 'donald@example.com',
+      phone: '+1 321 654 9870',
+      age: 41,
+    },
+    {
+      id: 4,
+      name: 'Donald Richardson',
+      email: 'donald@example.com',
+      phone: '+1 321 654 9870',
+      age: 41,
+    },
+    {
+      id: 5,
+      name: 'Donald Richardson',
+      email: 'donald@example.com',
+      phone: '+1 321 654 9870',
+      age: 41,
+    },
+    {
+      id: 6,
+      name: 'Donald Richardson',
+      email: 'donald@example.com',
+      phone: '+1 321 654 9870',
+      age: 41,
+    },
+    {
+      id: 7,
+      name: 'Donald Richardson',
+      email: 'donald@example.com',
+      phone: '+1 321 654 9870',
+      age: 41,
+    },
+    {
+      id: 10,
+      name: 'John doe',
+      email: 'Johndoe@example.com',
+      phone: '+91 90239 93821',
+      age: 41,
+    },
+  ];
+
+  editPerson(e: Event, id: number) {
+    e.preventDefault();
+    const selectedPerson = this.data.find((person) => person.id === id);
+
+    if (selectedPerson) {
+      this.selectedData = selectedPerson;
+    }
+    this.isModel = !this.isModel;
+  }
+
+  closeModel(e: Event) {
+    e.preventDefault();
+    this.isModel = !this.isModel;
+  }
+
+  updateModelBtn(e: Event) {
+    e.preventDefault();
+    this.closeModel(e);
+  }
 }
---
+++ src/app/app.model.ts
@@ -0,0 +1,9 @@
+export class Person {
+  constructor(
+    public id: number,
+    public name: string,
+    public email: string,
+    public phone: string,
+    public age: number
+  ) {}
+}
